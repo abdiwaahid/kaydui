f0c8aa343c988e5cf3f5659e6a59820505b3908b
abdiwaahid <cabdulwaaxid6@gmail.com>
2025-07-12 14:47:25 +0300

    deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://abdiwaahid.github.io/',
+  base:'/kaydui',
   markdown: {
     syntaxHighlight: "prism",
   },
