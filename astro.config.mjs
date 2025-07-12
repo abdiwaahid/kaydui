b9c2cee5653a80704ad24049da12bff620d72006
abdiwaahid <cabdulwaaxid6@gmail.com>
2025-07-12 22:35:04 +0300

    github pages base href

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site:'https://abdiwaahid.github.io/',
-  base:'/',
+  base:'/kaydui',
   markdown: {
     syntaxHighlight: "prism",
   },
